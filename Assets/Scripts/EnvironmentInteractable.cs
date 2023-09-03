56f8ca44f0f19e71ec1ee522fba7965a0d650c5f
Rezende <rezendedev@gmail.com>
2023-09-03 11:53:26 -0300

    feat: env interactables base behaviour

---
+++ Assets/Scripts/EnvironmentInteractable.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnvironmentInteractable : MonoBehaviour
+{
+
+    public bool IsOneTimeInteractable = false;
+
+    private BoxCollider2D _boxCollider2D;
+    public GameObject _uiElement;
+
+    private void Awake()
+    {
+        _boxCollider2D = this.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_uiElement != null)
+            _uiElement.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (_uiElement != null)
+            _uiElement.SetActive(false);
+    }
+
+    private void DeactivateBoxCollider() => _boxCollider2D.enabled = false;
+
+}
